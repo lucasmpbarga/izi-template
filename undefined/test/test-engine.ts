2946a3fe90d79e395ae05827b0c43426550b88a5
Lucas Barga <lucasmpbarga@gmail.com>
2024-02-10 17:43:42 -0300

    1.0.11

--- undefined/test/test-engine.ts
+++
@@ -1,9 +0,0 @@
-
-import { testRecommendation } from './test-recommendation';
-import { testReferenceRange } from './test-reference-range';
-
-export const testEngine = {
-  getReferenceRange: testReferenceRange,
-  getRecommendation: testRecommendation,
-};
-
