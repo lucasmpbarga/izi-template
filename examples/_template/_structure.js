d05deb2b20376b4601c39cf2fa9d4bee866f6e78
Lucas Barga <lucasmpbarga@gmail.com>
2024-02-10 20:55:58 -0300

    up template example

---
+++ examples/_template/_structure.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { makeNutrientEngineTemplate } = require('./nutrient-engine-template.js');
+const {
+  makeNutrientRecommendationTemplate,
+} = require('./nutrient-recommendation-template.js');
+const {
+  makeNutrientReferenceRangeTemplate,
+} = require('./nutrient-reference-range-template.js');
+
+const makeStructure = (projectName) => {
+  return {
+    [`${projectName}-engine.ts`]: makeNutrientEngineTemplate(projectName),
+    [`${projectName}-recommendation.ts`]:
+      makeNutrientRecommendationTemplate(projectName),
+    [`${projectName}-reference-range.ts`]:
+      makeNutrientReferenceRangeTemplate(projectName),
+  };
+};
+
+module.exports = {
+  makeStructure,
+};
---
+++ examples/_template/nutrient-reference-range-template.js
@@ -0,0 +1,46 @@
+function kebabCaseToCamelCase(inputString) {
+  return inputString.replace(/-([a-z])/g, (match, group) =>
+    group.toUpperCase(),
+  );
+}
+
+const makeNutrientReferenceRangeTemplate = (parameter) => {
+  const camelCaseParameter = kebabCaseToCamelCase(parameter);
+
+  return `
+  export const ${camelCaseParameter}ReferenceRange = () => {
+    let response = {
+      type: 'U/L',
+      min: 0,
+      max: 0,
+    };
+  
+    // EXAMPLE
+    // if (ageInDays >= 0 && ageInDays <= 14) {
+    //   response = {
+    //     ...response,
+    //     min: 134,
+    //     max: 518,
+    //   };
+    // } else if (ageInDays > 14 && ageInDays <= 365) {
+    //   response = {
+    //     ...response,
+    //     min: 134,
+    //     max: 518,
+    //   };
+    // } else {
+    //   response = {
+    //     ...response,
+    //     min: 40,
+    //     max: 150,
+    //   };
+    // }
+  
+    return response;
+  };  
+`;
+};
+
+module.exports = {
+  makeNutrientReferenceRangeTemplate,
+};
